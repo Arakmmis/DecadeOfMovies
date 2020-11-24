cb1e681b66239eb4206b257f8c953055dd05c0e2
abdelrahman.shaltoot <abdelrahman.shaltoot@almtar.com>
2020-11-25 01:42:04 +0200

    Removed unnecessary gradle lib and updated kotlin ver

--- app/src/main/java/com/movies/decade/businesslogic/FlickrApiService.kt
+++ app/src/main/java/com/movies/decade/businesslogic/FlickrApiService.kt
@@ -1,12 +1,9 @@
 package com.movies.decade.businesslogic
 
-import androidx.lifecycle.LiveData
 import com.movies.decade.businesslogic.models.ImagesResponse
 import com.movies.decade.utils.FLICKR_IMAGE_API
 import io.reactivex.Single
-import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface FlickrApiService {
